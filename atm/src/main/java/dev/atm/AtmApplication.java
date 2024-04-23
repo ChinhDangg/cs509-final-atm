ded0b0112bbe42a87198bbdfeaf6d7a187ed5a7a
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2024-04-23 19:30:58 -0400

    Update

--- atm/src/main/java/dev/atm/AtmApplication.java
+++ atm/src/main/java/dev/atm/AtmApplication.java
@@ -7,6 +7,7 @@ import dev.atm.Enum.Role;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
@@ -14,34 +15,31 @@ import java.util.Scanner;
 @SpringBootApplication
 public class AtmApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AtmApplication.class, args);
-	}
+		ApplicationContext ctx = SpringApplication.run(AtmApplication.class, args);
+		AccountService accountService = ctx.getBean(AccountService.class);
+		Scanner scanner = new Scanner(System.in);
+		int count = 0;
+		while (count < 100) {
+			String[] loginInfo = getLoginInfo(scanner);
+			if (loginInfo == null)
+				continue;
+			Account acc = accountService.checkLogin(loginInfo[0], loginInfo[1]);
+			if (!checkLogin(acc))
+				continue;
 
-	@Bean
-	CommandLineRunner commandLineRunner(AccountService accountService) {
-		return args -> {
-			Scanner scanner = new Scanner(System.in);
-			while (true) {
-				String[] loginInfo = getLoginInfo(scanner);
-				if (loginInfo == null)
-					continue;
-				Account acc = accountService.checkLogin(loginInfo[0], loginInfo[1]);
-				if (!checkLogin(acc))
-					continue;
-
-				if (acc.getCurrentRole() == Role.Admin) {
-					AdminMenu a = new AdminMenu(accountService);
-					a.runMenu(scanner);
-				}
-				else {
-					ClientMenu c = new ClientMenu(accountService);
-					c.runMenu(scanner, acc.getId());
-				}
+			if (acc.getCurrentRole() == Role.Admin) {
+				AdminMenu a = new AdminMenu(accountService);
+				a.runMenu(scanner);
 			}
-		};
+			else {
+				ClientMenu c = new ClientMenu(accountService);
+				c.runMenu(scanner, acc.getId());
+			}
+			count++;
+		}
 	}
 
-	private boolean checkLogin(Account account) {
+	private static boolean checkLogin(Account account) {
 		if (account == null) {
 			System.out.println("Wrong login or pin");
 			return false;
@@ -53,7 +51,7 @@ public class AtmApplication {
 		return true;
 	}
 
-	private String[] getLoginInfo(Scanner scanner) {
+	private static String[] getLoginInfo(Scanner scanner) {
 		System.out.println();
 		System.out.print("Enter login: ");
 		String login = scanner.nextLine();
