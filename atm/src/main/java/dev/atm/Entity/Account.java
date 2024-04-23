10246e57649a76e9f27372fe5ac237de994603a6
Chinh Dang <61719308+ChinhDangg@users.noreply.github.com>
2024-04-23 18:33:42 -0400

    Update

---
+++ atm/src/main/java/dev/atm/Entity/Account.java
@@ -0,0 +1,58 @@
+package dev.atm.Entity;
+
+import dev.atm.Enum.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    @Column(name = "Login", nullable = false)
+    private String login;
+    @Column(name = "Pin", nullable = false)
+    private int pin;
+    @Column(name = "HoldersName", nullable = false)
+    private String holdersName;
+    @Column(name = "Balance", precision = 10, scale = 2, nullable = false)
+    private BigDecimal balance;
+    @Column(name = "Status", nullable = false)
+    private boolean status;
+    @Column(name = "Role", nullable = false)
+    private boolean role;
+
+    public Account(String login, int pin, String holdersName, BigDecimal balance, boolean status, boolean role) {
+        this.login = login;
+        this.pin = pin;
+        this.holdersName = holdersName;
+        this.balance = balance;
+        this.status = status;
+        this.role = role;
+    }
+
+    public Account(String login, int pin, String holdersName, boolean status, boolean role) {
+        this.login = login;
+        this.pin = pin;
+        this.holdersName = holdersName;
+        this.status = status;
+        this.role = role;
+    }
+
+    public Role getCurrentRole() {
+        return (role) ? Role.Admin : Role.Client;
+    }
+
+    public String getCurrentStatus() {
+        return (status) ? "Active" : "Disabled";
+    }
+}
